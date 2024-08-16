813fb1045bc41bbc52a4daf344f70eca63d3332a
Jishan Arzoo <150323168+JishanArzoo@users.noreply.github.com>
2024-08-16 18:44:18 +0530

    Delete src directory

--- src/utils/ApiResponse.js
+++
@@ -1,9 +0,0 @@
-class ApiResponse {
-    constructor(statusCode, data, success = true){
-        this.statusCode = statusCode,
-        this.data = data,
-        this.success = success
-    }
-}
-
-export {ApiResponse}
