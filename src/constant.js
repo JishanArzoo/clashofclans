6bf4cf62f01414b53c80599597a5040bd7f8d5c9
Jishan Arzoo <150323168+JishanArzoo@users.noreply.github.com>
2024-08-16 14:40:04 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
 export const cocBaseURI = "https://api.clashofclans.com/v1" 
 export const AuthHeaderCOC = {
     headers: {
-        Authorization : `bearer ${process.env.COC_ACCESS_TOKEN}`
+        Authorization : `Bearer ${process.env.COC_ACCESS_TOKEN}`
     }
 }
 export const options  = {
